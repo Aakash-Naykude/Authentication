b03c2c466017cead11bc7a1ab573399461e75f44
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-12-03 22:58:08 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const {signup, signin} = require("./controllers/auth.controller")
+const newitemController = require('./controllers/newitem.controller')
 app.post("/signup", signup)
 app.post("/signin", signin)
+app.use("/newitem", newitemController)
 module.exports = app;
